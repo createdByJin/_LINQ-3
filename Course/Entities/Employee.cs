d151ac7ce237686afd3b2d8da475f4dfa948d49e
Danilo Fontes <danilo_f_de-carvalho@estudante.sc.senai.br>
2020-03-07 00:28:39 -0300

    exercicio 2 LINQ

---
+++ Course/Entities/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Course.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ",\n"
+                + Email
+                + ",\n"
+                + Salary.ToString("f2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Course.Entities;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+            Console.Write("Enter salary: ");
+            double filterSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            List<Employee> list = new List<Employee>();
+
+            using(StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] vet = sr.ReadLine().Split(',');
+                    string name = vet[0];
+                    string email = vet[1];
+                    double salary = double.Parse(vet[2], CultureInfo.InvariantCulture);
+                    list.Add(new Employee(name, email, salary));
+                }
+            }
+
+            var query1 = list.Where(p => p.Salary > filterSalary).OrderBy(p => p.Email).Select(p => p.Email);
+
+            var query2 = list.Where(p => p.Name[0] == 'M').Sum(p => p.Salary);            
+
+            Console.WriteLine("\nEmail of people whose salary is more than " + filterSalary.ToString("f2", CultureInfo.InvariantCulture) + ":");
+
+            foreach(string email in query1)
+            {
+                Console.WriteLine(email);
+            }
+
+            Console.Write("\nSum of salary of people whose name starts with 'M': " + query2.ToString("f2", CultureInfo.InvariantCulture));
+        }
+    }
+}
